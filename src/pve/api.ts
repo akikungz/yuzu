590c180316dce0f478706224ad4db137b4609593
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2026-04-05 21:55:01 +0700

    refactor(api): streamline PVE API client by removing SSL verification and using fetch

--- src/pve/api.ts
+++ src/pve/api.ts
@@ -1,19 +1,16 @@
 import { env } from "@yuzu/infrastructure/config/env";
 import createClient from "openapi-fetch";
-import { Agent } from "undici";
 import type { paths } from "./type";
-
-// Disable SSL certificate verification for self-signed certificates in development
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 export const pveApi = createClient<paths>({
 	baseUrl: `${env.PVE_API_URL}`,
 	headers: {
 		Authorization: `PVEAPIToken=${env.PVE_API_TOKEN_ID}=${env.PVE_API_TOKEN_SECRET}`,
 	},
-	dispatcher: new Agent({
-		connect: {
-			rejectUnauthorized: false,
-		},
-	}),
+	fetch: (request) =>
+		fetch(request, {
+			tls: {
+				rejectUnauthorized: false,
+			},
+		}),
 });
